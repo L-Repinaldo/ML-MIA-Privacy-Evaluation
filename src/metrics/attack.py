ba1d2e70e8c2c827db5628437ccb5dad4f8ade48
L-Repinaldo <lucasrepinaldo0811@gmail.com>
2026-08-09 20:31:15 -0300

    refactor(metrics): reorganize attack and utility evaluation

--- src/metrics/attack.py
+++ src/metrics/attack.py
@@ -1,11 +1,12 @@
-from sklearn.metrics import confusion_matrix, accuracy_score
+from sklearn.metrics import confusion_matrix, accuracy_score, f1_score, precision_score, recall_score
 
 
 def compute_attack_metrics(y_true, y_pred):
-    """
-    Calcula métricas básicas da eficacia das classificações do ataque.
-    """ 
 
+    attack_f1 = f1_score(y_true=y_true, y_pred=y_pred, zero_division=0)
+    attack_precision = precision_score(y_true=y_true, y_pred=y_pred, average='weighted', zero_division=0)
+    attack_recall = recall_score(y_true=y_true, y_pred=y_pred, average='weighted', zero_division=0)
+ 
     tn, fp, fn, tp = confusion_matrix(y_true= y_true, y_pred= y_pred).ravel().tolist()
 
     member_acc =  ( tp  / (tp + fn) )
@@ -16,6 +17,9 @@ def compute_attack_metrics(y_true, y_pred):
 
     return {
         "attack_acc": attack_acc,
+        "attack_f1": attack_f1,
+        "attack_precision": attack_precision,
+        "attack_recall": attack_recall,
         "member_acc": member_acc,
         "non_member_acc": non_member_acc,
         "advantage": advantage
